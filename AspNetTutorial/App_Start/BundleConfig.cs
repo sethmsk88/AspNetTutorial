c9337394bf42ce99f43496332c29fb1637ff34ff
Seth Kerr <seth.kerr@talgov.com>
2019-07-26 16:20:35 -0400

    Add a theme and view called Movies

--- AspNetTutorial/App_Start/BundleConfig.cs
+++ AspNetTutorial/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace AspNetTutorial
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-pulse.css",
                       "~/Content/site.css"));
         }
     }
---
+++ AspNetTutorial/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetTutorial.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            return View(movie); // return the movie object to the view
+            // return Content("Testing"); // Show only the text I put in the helper method Content
+            // return HttpNotFound(); // show a 404 page
+            // return new EmptyResult(); // return nothing
+            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" }); // redirect to another page with URL arguments
+        }
+    }
+}
